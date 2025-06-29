0295030e6c1b7ff45953c6254c16f47dd3b05ea9
nusaifpk <nusaifputhiyakath1@gmail.com>
2025-06-29 15:37:42 +0530

    first commit

---
+++ app/components/PDFFlipbook.jsx
@@ -0,0 +1,104 @@
+'use client';
+
+import React, { useEffect, useState } from 'react'
+import HTMLFlipBook from 'react-pageflip';
+import { BounceLoader } from 'react-spinners';
+import * as pdfjsLib from 'pdfjs-dist/build/pdf';
+pdfjsLib.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjsLib.version}/pdf.worker.min.js`;
+
+
+
+
+const PDFFlipbook = ({ pdfurl}) => {
+
+    const [loading, setLoading] = useState(true);
+    const [dimensions, setDimensions] = useState({width: 500, height: 700});
+    const [pages, setpages] = useState([])
+
+    // flipbook responsive
+    useEffect(() => {
+        const updateSize = () => {
+            const screenWidth = window.innerWidth;
+            if(screenWidth < 640) {
+                setDimensions({width: 300, height: 420});
+            } else if (screenWidth < 768){
+                setDimensions({width: 400, height: 560})
+            } else {
+                setDimensions({width: 450, height: 630})
+            }
+        };
+
+        updateSize();
+        window.addEventListener('resize', updateSize);
+        return () => window.removeEventListener('resize', updateSize)
+    },[])
+
+
+    // load and read pdf
+    useEffect(() => {
+        const loadPDF = async () => {
+            try {
+                const loadingTask = pdfjsLib.getDocument(pdfurl);
+                const pdf = await loadingTask.promise;
+                const pagesImages = [];
+
+                for (let i = 1; i <= pdf.numPages; i++){
+                    const page = await pdf.getPage(i)
+                    const viewport = page.getViewport({ scale: 1.5})
+                    const canvas = document.createElement('canvas')
+                    const context = canvas.getContext('2d')
+                    canvas.height = viewport.height
+                    canvas.width = viewport.width
+
+                    await page.render({ canvasContext: context, viewport}).promise;
+                    const imageUrl = canvas.toDataURL();
+                    pagesImages.push(imageUrl);
+                }
+
+                setpages(pagesImages)
+            } catch (error) {
+                console.error('Failed to load pdf', error)
+            } finally {
+                setLoading(false)
+            }
+        }
+        loadPDF();
+    }, [pdfurl])
+
+  return (
+    <div className='flex justify-center items-center min-h-screen bg-white p-4'>
+        {loading ? (
+            <div className='flex flex-col justify-center items-center space-y-4'>
+                <h1 className='text-xl text-gray-600 font-thin'>Loading...</h1>
+                <BounceLoader size={40} color='#2563EB' />
+            </div>
+        ) : (
+            // swiping or touch area
+            <div className='p-2 sm:p-0 bg-white rounded-md shadow-lg touch-pan-x'>
+                <HTMLFlipBook
+                    width={dimensions.width}
+                    height={dimensions.height}
+                    showCover={true}
+                    mobileScrollSupport={true}
+                    className='rounded-md'
+                >
+                    {pages.map((img, index) => (
+                        <div
+                            key={index}
+                            className='bg-white flex items-center justify-center overflow-hidden'
+                        >
+                            <img 
+                                src={img} 
+                                alt={`Page ${index + 1}`}
+                                className='w-full h-full object-contain' 
+                            />
+                        </div>
+                    ))}
+                </HTMLFlipBook>
+            </div>
+        )}
+    </div>
+  )
+}
+
+export default PDFFlipbook
